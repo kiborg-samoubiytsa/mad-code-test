45f7dec15c494046eea5f7f96e53808a48b23405
kiborg-samoubiytsa <kentsis2005@gmail.com>
2024-06-02 17:42:08 +0300

    first commit

--- src/components/TestConfirmation.tsx
+++ src/components/TestConfirmation.tsx
@@ -19,36 +19,20 @@ export const TestConfirmation = () => {
       { questionText: "Сколько собак в конюшне?", type: "short-answer" },
       { questionText: "Сколько коней в псарне?", type: "long-answer" },
       {
-        questionText: "Какие из этих челов крутые",
+        questionText: "Кто?",
         type: "multiple-choice",
-        choices: [
-          { text: "иван золо" },
-          { text: "крипер2004" },
-          { text: "каша" },
-        ],
+        choices: [{ text: "Он" }, { text: "Виталий" }, { text: "Никто" }],
       },
       {
-        questionText: "Какой из них самый крутой",
+        questionText: "Сколько?",
         type: "single-choice",
-        choices: [
-          { text: "иван золо" },
-          { text: "крипер2004" },
-          { text: "каша" },
-        ],
-      },
-      {
-        questionText: "Какой из них самый крутой",
-        type: "single-choice",
-        choices: [
-          { text: "иван золо" },
-          { text: "крипер2004" },
-          { text: "каша" },
-        ],
+        choices: [{ text: "семь" }, { text: "банан" }, { text: "синий" }],
       },
     ];
     const now = new Date();
+    //можно получать из запроса, это просто мок понятное дело
     dispatch(setIsTimeLimited(true));
-    dispatch(setMaxTimeMinutes(maxTimeMinutes)); //можно получать из запроса
+    dispatch(setMaxTimeMinutes(maxTimeMinutes));
     dispatch(setEndTime(now.getTime() + maxTimeMinutes * 1000 * 60));
     dispatch(setQuestions(testQuestions as QuestionWithAnswer[]));
     dispatch(startTest());
@@ -73,20 +57,12 @@ export const TestConfirmation = () => {
       </style>
       <div className="test_confirmation">
         <p className="test_description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-          doloribus iusto nobis perferendis officiis quidem, repellendus error
-          est quibusdam neque? Possimus facilis sint error voluptatibus non
-          repudiandae, veniam fugiat quis. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Harum doloribus iusto nobis perferendis
-          officiis quidem, repellendus error est quibusdam neque? Possimus
-          facilis sint error voluptatibus non repudiandae, veniam fugiat quis.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-          doloribus iusto nobis perferendis officiis quidem, repellendus error
-          est quibusdam neque? Possimus facilis sint error voluptatibus non
-          repudiandae, veniam fugiat quis. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Harum doloribus iusto nobis perferendis
-          officiis quidem, repellendus error est quibusdam neque? Possimus
-          facilis sint error voluptatibus non repudiandae, veniam fugiat quis.
+          Невероятно долгий текст описывающий что будет в тесте. УУУУ совсем уже
+          с этими ЕГЭ отупели, выбирают как обезьянки из трех вариантов(цитата
+          если что) Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Dignissimos ipsa velit nam aperiam doloribus asperiores quaerat ea
+          provident voluptates blanditiis quis, distinctio deleniti est, harum
+          non repudiandae modi? Sed, optio.
         </p>
         <Button variant="flat" size="lg" onClick={() => configureTest()}>
           Подтвердить
